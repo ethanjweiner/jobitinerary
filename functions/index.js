561c1276d1614421cf1328530b502d8590309f98
Ethan Weiner <ethanweiner20@gmail.com>
2021-10-14 22:58:25 -0400

    Configure emulators

--- functions/index.js
+++ functions/index.js
@@ -7,35 +7,22 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin
-    .firestore()
-    .collection("messages")
-    .add({ original: original });
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
+// When date is changed, push the visit ID to the associated day
+// When employee or customer is changed, push the visit ID to the associated day
+exports.onVisitUpdate = functions.firestore
+  .document("/companies/{companyID}/visits/{visitID}")
+  .onUpdate((change, context) => {
+    const newVisit = change.after.data();
+    const previousVisit = change.before.data();
 
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore
-  .document("/messages/{documentId}")
-  .onCreate((snap, context) => {
-    // Grab the current value of what was written to Firestore.
-    const original = snap.data().original;
-
-    // Access the parameter `{documentId}` with `context.params`
-    functions.logger.log("Uppercasing", context.params.documentId, original);
-
-    const uppercase = original.toUpperCase();
-
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    return snap.ref.set({ uppercase }, { merge: true });
+    // Handle all cases of changed dates
+    if (newVisit.data.date == oldVisit.data.date)
+      functions.logger.log(newVisit, previousVisit);
   });
+
+// Delete associated day(s) if necessary
+exports.onVisitDelete = functions.firestore
+  .document("/companies/{companyID}/visits/{visitID}")
+  .onDelete();
+
+// Create a final cloud function to perform regular sanitization of days that no longer contain any content
