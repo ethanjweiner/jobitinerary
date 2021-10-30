dd9ceacaee46e4a86226a50d8513067292b39261
Ethan Weiner <ethanweiner20@gmail.com>
2021-10-30 15:01:19 -0400

    Restructure expenses list

--- src/components/lists/ExpensesInfinite.vue
+++ src/components/lists/ExpensesInfinite.vue
@@ -1,43 +1,25 @@
 <template>
-  <div style="height: 100%;">
-    <ion-card-header>
-      <ion-card-title>
-        {{ title }}
-      </ion-card-title>
-      <ion-searchbar
-        class="ion-text-start"
-        v-model="searchText"
-        placeholder="Search by expense name, date, or cost"
-      ></ion-searchbar>
-    </ion-card-header>
-    <ion-card-content>
-      <InfiniteList
-        :key="searchText"
-        :pushQuantity="6"
-        :searchFilter="searchText"
-        :dbRef="dbRef"
-        orderByParam="date"
-      >
-        <!-- Task: Supply an expense object (rather than document data) to the Expense component -->
-        <template v-slot:item="itemProps">
-          <ExpenseItem :expense="itemProps.item" :showDate="true" />
-        </template>
-      </InfiniteList>
-    </ion-card-content>
-  </div>
+  <SearchToolbar disableAdd title="Expenses" v-model="searchText" />
+  <InfiniteList
+    :key="searchText"
+    :pushQuantity="6"
+    :searchFilter="searchText"
+    :dbRef="dbRef"
+    orderByParam="date"
+    type="list"
+  >
+    <template v-slot:item="itemProps">
+      <ExpenseItem :expense="itemProps.item" :showDate="true" />
+    </template>
+  </InfiniteList>
 </template>
 
 <script lang="ts">
-import {
-  IonCardHeader,
-  IonCardTitle,
-  IonSearchbar,
-  IonCardContent,
-} from "@ionic/vue";
 import { ref } from "@vue/reactivity";
 
 import InfiniteList from "@/components/lists/InfiniteList.vue";
 import ExpenseItem from "./items/ExpenseItem.vue";
+import SearchToolbar from "@/components/inputs/SearchToolbar.vue";
 
 export default {
   name: "Expenses Infinite",
@@ -53,12 +35,9 @@ export default {
     };
   },
   components: {
-    IonCardHeader,
-    IonCardTitle,
-    IonSearchbar,
-    IonCardContent,
     InfiniteList,
     ExpenseItem,
+    SearchToolbar,
   },
 };
 </script>
