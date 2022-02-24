dc43d5d70fdde30fa089021313033cf97c94ca4f
Ethan Weiner <ethanweiner20@gmail.com>
2022-02-23 20:25:00 -0500

    Ionic v6 dependency updates

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   transform: {
-    '^.+\\.vue$': 'vue-jest'
+    "^.+\\.vue$": "vue-jest",
   },
-  transformIgnorePatterns: ['/node_modules/(?!@ionic/vue|@ionic/vue-router)']
-}
+  transformIgnorePatterns: [
+    "/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|ionicons)",
+  ],
+};
