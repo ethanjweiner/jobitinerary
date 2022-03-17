c6781b0f750a6106edb244491607799a8fe2df32
Ethan Weiner <ethanweiner20@gmail.com>
2022-03-16 20:29:39 -0400

    Move alert to app

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,45 @@
   <ion-app>
     <ion-router-outlet />
   </ion-app>
+  <!-- Global Alerts -->
+  <ion-toast
+    :is-open="alertIsOpen"
+    :message="store.alert.message"
+    :color="store.alert.color"
+    :duration="5000"
+    @didDismiss="setAlertOpen(false)"
+  >
+  </ion-toast>
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from "@ionic/vue";
-import { defineComponent } from "vue";
+import { IonApp, IonRouterOutlet, IonToast } from "@ionic/vue";
+import { defineComponent, ref, watch } from "vue";
+
+import store from "./store";
 
 export default defineComponent({
   name: "App",
   components: {
     IonApp,
     IonRouterOutlet,
+    IonToast,
   },
   setup() {
     document.body.classList.remove("dark");
+
+    const alertIsOpen = ref(false);
+    const setAlertOpen = (state: boolean) => (alertIsOpen.value = state);
+
+    watch(store.alert, () => {
+      if (store.alert.message) setAlertOpen(true);
+    });
+
+    return {
+      alertIsOpen,
+      setAlertOpen,
+      store,
+    };
   },
 });
 </script>
